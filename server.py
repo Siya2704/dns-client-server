7b06d8cf7640b397b228c09bb531f345c8b4e4c6
Siya Dhokane <dhokaness19.comp@coep.ac.in>
2021-11-10 05:35:26 +0000

    updated cache

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
+
 from library import *
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.bind(('127.0.0.1',53))
 sock2 = socket(AF_INET, SOCK_DGRAM)
 
 def entry_cache(query, response, start, number_response):
-	hostname,t,c = get_query_details(query)
+	hostname,t,c,start = get_query_details(query)
 	try:
 		with open("cache.json", 'a') as fp:
-			types = ['A','AAAA','NS','MX','CNAME','SOA','TXT']
+			types = [1,2,5,6,15,16,28]
 			if t in types:
-				print("...Writing to cache")
+				print("**Writing to cache**")
 			for i in range(number_response):
 				type = response[start+3]
 				clas = response[start+5]
@@ -19,57 +20,39 @@ def entry_cache(query, response, start, number_response):
 				toe = int(round(time.time()))
 				length = response[start+11]
 				#toe=time of entry
-				if type == 1 and t == 1:#A
-					name,data = get_ipv4(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
+				if type in types:
+					data = response[start:start+length+12]
+					lst = []
+					for k in data:
+						lst.append(k)
+					record = {"query":hostname,"type":t,"act_type":type,"class":clas,"ttl":ttl,"toe":toe,"data":lst}
 					json.dump(record,fp)
 					fp.write('\n')
-				elif type == 28 and t == 28:#AAAA
-					name,data = get_ipv6(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
-					json.dump(record,fp)
-					fp.write('\n')
-				elif type == 2 and t == 2:#NS
-					name,data = get_NS(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
-					json.dump(record,fp)
-					fp.write('\n')	
-				elif type == 5 and t == 5:#CNAME
-					name,data = get_NS(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
-					json.dump(record,fp)
-					fp.write('\n')
-				elif type == 6 and t == 6:#SOA
-					name,pns,ram,sn,rfi,rti,el,mt = get_SOA(response,start)
-					data = [pns,ram,sn,rfi,rti,el,mt]
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
-					json.dump(record,fp)
-					fp.write('\n')
-				elif type == 15 and t == 15:#MX
-					name,data = get_MX(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":data}
-					json.dump(record,fp)
-					fp.write('\n')
-				elif type == 16 and t == 16:#TXT
-					name,txt = get_TXT(response,start)
-					record = {"query":hostname,"name":name,"type":type,"class":clas,"ttl":ttl,"toe":toe,"data":txt}
-					json.dump(record,fp)
-					fp.write('\n')	
 					
 				start += length + 12
 	except Exception as e:
-		print(e)
-		print("..unable to write to cache")
+		print("**unable to write to cache**")
 
-def lookup_cache(name, type, clas):
-	list  = []
-	print("Looking in cache")
+def lookup_cache(name, type, clas,query):
+	x = query[:2] + bytes('\x08','utf-8')+query[3:7]
+	y = bytes('','utf-8')
+	count = 0
+	print("**Searching in cache**")
 	with open("cache.json", 'r') as fp:
 		for line in fp.readlines():
 			dct = json.loads(line)
 			if(dct['query'] == name and dct['type'] == type and dct['class'] == clas and int(round(time.time()))< dct['toe']+dct['ttl']):
-				list.append((dct["name"],dct["type"],dct["data"]))
-	return list
+				count += 1
+				lst = dct["data"]
+				for k in lst:
+					y+= bytes(chr(k),'iso-8859-1')
+
+	x += bytes(chr(count)+"\x00\x00" + "\x00\x00",'iso-8859-1')
+	x+=query[12:]
+	x += y
+	if(count == 0):
+		return 0
+	return x
 	
 def update_cache():
 	items_to_keep = []
@@ -89,7 +72,7 @@ def dns_response(ip,query):
 	sock2.sendto(query, (ip, 53))
 	response, addr2 = sock2.recvfrom(2048)
 	number_queries, number_response, number_authority, number_additional, rcode= data_packet_dns(response)
-	start = len(query)
+	a_dc,b_dc,c_dc,start = get_query_details(query)
 	if(number_response):
 		entry_cache(query, response, len(query), number_response)
 		return response,addr2,True
@@ -121,22 +104,6 @@ def iterate_query(root,query):#for iterative query
 		else:
 			return iterate_query(res, query)
 	return -1
-			
-def open_resolv():
-	try:
-		resolvconf = open(os.path.abspath("/etc/resolv.conf"),'r')
-		for line in resolvconf.readlines():
-			if '#' in line:
-				pass
-			elif 'nameserver' in line:
-				resolver = line.split('nameserver ',1)[1]
-				resolver = resolver.split('\n',1)[0]
-				resolvconf.close()
-				return resolver
-	except Exception as e:
-		print(e)
-		resolvconf.close()
-		exit()
 
 def main():	
 	#continuously updating cache
@@ -144,35 +111,38 @@ def main():
 	th.start()
 	while True:
 		query, addr = sock.recvfrom(2048)
+		print("Query received: ",query)
 		#look cache
-		name,type,clas = get_query_details(query)
-		x = lookup_cache(name, type, clas)
-		if(len(x) != 0):
-			lst = str(['found',x])
-			sock.sendto(lst.encode(),addr)
+		name,typ,clas,start = get_query_details(query)
+		x = lookup_cache(name, typ, clas,query)
+		if(x != 0):
+			print("**Found in Cache**")
+			sock.sendto(x,addr)
 			continue
+		print("**Not Found in Cache**")
 		tuple_data_dns = struct.unpack('!HHHHHH', query[:12])
 		flags = tuple_data_dns[1] 
 		rd = (flags & 256) != 0
 		if(rd == 1):
 			#recursive
 			print("***Recursive Query***")
-			dns = open_resolv()
+			dns = "8.8.8.8"
 			sock2.sendto(query, (dns, 53))
 			response, addr2 = sock2.recvfrom(2048)
-			start = len(query)#start of answer
 			tuple_data_dns = struct.unpack('!HHHHHH', response[:12])
 			number_response = tuple_data_dns[3]
-			entry_cache(query, response, start, number_response)
+			if number_response >0:
+				entry_cache(query, response, start, number_response)
 			sock.sendto(response,addr)
 		else:
 			#iterative
 			print("***Iterative Query***")
-			root = [("ICANN",'199.7.83.42')]#ICANN server
+			root = [("l.root-servers.net",'199.7.83.42')]#ICANN
 			got = False
 			try:
 				response = iterate_query(root, query)
-			except:
+			except Exception as e:
+				print(e)
 				response = -1
 			if(response == -1):
 				print("Cannot resolve");
@@ -184,4 +154,3 @@ if __name__ == "__main__":
 	main()
 			
 			
-
